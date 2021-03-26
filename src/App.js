16d739a8b4121153805cf30180e98fa5c1d7a492
Alan Kow <alankow11@gmail.com>
2021-03-26 15:56:27 +0200

    add array of notes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Note from "./components/Note"
+import Notes from "./components/Notes"
 
 
-function App(){
-    return [
-    Header(),
-    Footer(),
-    Note()
-    ]
+function App() {
+    return (
+    <div>
+        <Header />
+        <Footer />
+        {Notes.map((Notes) => {
+            return (
+                <Note 
+                    key = {Notes.key}
+                    title = {Notes.title}
+                    content = {Notes.content}
+                /> )
+        })}
+    </div> 
+    )
 }
 
 export default App;
